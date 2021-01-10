ff8261605d8d385a3b8111a723fdb7684854ae17
Sean Cannon <seancannon@gmail.com>
2021-01-10 11:49:43 -0500

    🏷  build tags page and query

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -83,3 +83,9 @@ exports.getStoreBySlug = async (req, res, next) => {
     if (!store) return next();
     res.render('store', { store, title: store.name });
 };
+
+exports.getStoresByTag = async (req, res) => {
+    const tags = await Store.getTagsList();
+    const { tag } = req.params;
+    res.render('tag', { tags, title: 'Tags', tag });
+};
--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,7 @@ router.post(
 
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
 module.exports = router;
